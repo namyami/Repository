337bfe5ad9af589a824adbf0d4562dffdc8b8f35
nam <164773869+namyami@users.noreply.github.com>
2025-05-18 10:35:10 +0700

    Add files via upload

---
+++ Reupload/judghementday2/fallen.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b9e9f0c6dc1e2908243f56e38c1c8866.lua"))()
---
+++ Reupload/judghementday2/fonts.lua
@@ -0,0 +1,1 @@
+print"Hello, you are now being tracked by the judge and all your information is now publicly available and will be used against you."
